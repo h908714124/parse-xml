b45cb852a2304095926c7974822c752ea9575a9f
h908714124 <kraftdurchblumen@gmx.de>
2021-02-14 09:04:04 +0100

    formatting

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@ using namespace tinyxml2;
 
 int main(int argc, char *argv[])
 {
-	static const char* xml =
-		"<?xml version=\"1.0\"?>"
-		"<!DOCTYPE PLAY SYSTEM \"play.dtd\">"
-		"<PLAY>"
-		"<TITLE>A Midsummer Night's Dream</TITLE>"
-		"</PLAY>";
+    static const char* xml =
+      "<?xml version=\"1.0\"?>"
+      "<!DOCTYPE PLAY SYSTEM \"play.dtd\">"
+      "<PLAY>"
+      "<TITLE>A Midsummer Night's Dream</TITLE>"
+      "</PLAY>";
     XMLDocument doc;
-	doc.Parse( xml );
+    doc.Parse( xml );
     XMLElement* titleElement = doc.FirstChildElement( "PLAY" )->FirstChildElement( "TITLE" );
-	const char* title = titleElement->GetText();
-	printf( "Name of play (1): %s\n", title );
+    const char* title = titleElement->GetText();
+    printf( "Name of play (1): %s\n", title );
     return 0;
 }
